ac8ce28e1fdfacb41b9afd396e7df3cc409f6b01
Christine Cua <cloudycua@gmail.com>
2019-01-30 16:56:07 +0000

    Automatically backed up by Learn

--- array.js
+++ array.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function createArray(){
-  return ["snickers", "hundred grand", "kitkat", "skittles"]
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
 }
+
+var evenNumber = new Array();
+
+evenNumbers[0] = 2;
+evenNumbers[1] = 4;
+evenNumbers[2] = 8;
